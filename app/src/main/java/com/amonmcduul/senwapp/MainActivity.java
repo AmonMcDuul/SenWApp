1f2503ed47db2b36bb41613f118f4cbbdfe3d52e
AmonMcDuul <joranomic@hotmail.com>
2020-02-01 17:45:37 +0100

    buttons klikbaar

--- app/src/main/java/com/amonmcduul/senwapp/MainActivity.java
+++ app/src/main/java/com/amonmcduul/senwapp/MainActivity.java
@@ -2,12 +2,32 @@ package com.amonmcduul.senwapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private Button bumbaButs;
+private Button tButts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bumbaButs = findViewById(R.id.bumbaButton);
+        bumbaButs.setOnClickListener(this);
+        tButts = findViewById(R.id.tButton);
+        tButts.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.bumbaButton:
+                setContentView(R.layout.activity_bumbazone);
+            break;
+            case R.id.tButton:
+                setContentView(R.layout.activity_tzone);
+        }
     }
 }
